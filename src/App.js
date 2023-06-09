064a81d48428c5d4c239494fdc842a4bf9c258cf
Drian-key <riskidrian1@gmail.com>
2023-06-09 22:47:52 +0700

    add suspense and react.lazy

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
-import { useEffect, useState } from "react";
+import React, { Suspense } from "react";
 import "./App.css";
-import Contact from "./components/Contact";
-import Header from "./components/Header";
-import Main from "./components/Main";
+
 import { ParallaxProvider } from "react-scroll-parallax";
 import Loading from "./components/Loading";
 
-function App() {
-  const [isLoading, setIsLoading] = useState(true);
+const Contact = React.lazy(() => import("./components/Contact"));
+const Header = React.lazy(() => import("./components/Header"));
+const Main = React.lazy(() => import("./components/Main"));
 
-  useEffect(() => {
-    // Simulasikan waktu pemuatan data
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 5000);
-  }, []);
+function App() {
   return (
-    <>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <ParallaxProvider>
-          <section id="header">
-            <Header />
-          </section>
-          <section className="relative">
-            <Main />
-            <Contact />
-            <footer className="bg-black text-white font-roboto text-center text-sm py-4 border-t-2 border-dark md:border-t-4">
-              @driannaird
-            </footer>
-          </section>
-        </ParallaxProvider>
-      )}
-    </>
+    <Suspense fallback={<Loading />}>
+      <ParallaxProvider>
+        <section id="header">
+          <Header />
+        </section>
+        <section className="relative">
+          <Main />
+          <Contact />
+          <footer className="bg-black text-white font-roboto text-center text-sm py-4 border-t-2 border-dark md:border-t-4">
+            @driannaird
+          </footer>
+        </section>
+      </ParallaxProvider>
+    </Suspense>
   );
 }
 
